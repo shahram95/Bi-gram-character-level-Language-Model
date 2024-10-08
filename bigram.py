54bcf9f74b1720e429cb8cc98ff8308bc1e67d6a
shahram95 <shahramnajam.neduet@gmail.com>
2024-10-08 02:24:03 -0400

    Improved lookup table visulization

--- bigram.py
+++ bigram.py
@@ -35,6 +35,14 @@ for w in words:
         N[ix1, ix2] += 1
 
 
-plt.imshow(N)
-plt.colorbar()
+itos = {i:s for s,i in stoi.items()}
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(28):
+    for j in range(28):
+        chstr = itos[i] + itos[j]
+        plt.text(j,i,chstr, ha="center", va="bottom", color="gray")
+        plt.text(j, i, N[i,j].item(), ha="center", va="top", color="gray")
+plt.axis("off")
 plt.show()
