a03d646270b13d04defd0e4c46415e9a559b6ae2
shahram95 <shahramnajam.neduet@gmail.com>
2024-10-08 02:19:33 -0400

    tensorfied the bigram lookup dictionary

--- bigram.py
+++ bigram.py
@@ -1,5 +1,6 @@
 # import any relevant libraries here
 import torch
+import matplotlib.pyplot as plt
 
 # Load dataset
 
@@ -8,7 +9,7 @@ words = open('data.txt', 'r').read().splitlines()
 
 # Starting bi-gram
 
-bigram_dictionary = {}
+'''bigram_dictionary = {}
 
 for w in words:
     chs = ['<S>'] + list(w) + ['<E>']                       # Hallucinating start and end characters
@@ -18,3 +19,22 @@ for w in words:
 
 #print(sorted(bigram_dictionary.items(), key = lambda kv : -kv[1]))
 
+'''
+
+N = torch.zeros((28, 28), dtype=torch.int32)
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i for i,s in enumerate(chars)}
+stoi['<S>'] = 26
+stoi['<E>'] = 27
+
+for w in words:
+    chs = ['<S>'] + list(w) + ['<E>']                       # Hallucinating start and end characters
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
+
+
+plt.imshow(N)
+plt.colorbar()
+plt.show()
