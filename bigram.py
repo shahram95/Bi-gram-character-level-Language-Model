1b4ab600b6a7817ca4f9ad2405eefe38b44a1476
shahram95 <shahramnajam.neduet@gmail.com>
2024-10-08 02:30:02 -0400

    Added common representation for start and end character

--- bigram.py
+++ bigram.py
@@ -21,28 +21,27 @@ for w in words:
 
 '''
 
-N = torch.zeros((28, 28), dtype=torch.int32)
+N = torch.zeros((27, 27), dtype=torch.int32)
 chars = sorted(list(set(''.join(words))))
-stoi = {s:i for i,s in enumerate(chars)}
-stoi['<S>'] = 26
-stoi['<E>'] = 27
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
 
 for w in words:
-    chs = ['<S>'] + list(w) + ['<E>']                       # Hallucinating start and end characters
+    chs = ['.'] + list(w) + ['.']                       # Hallucinating start and end characters
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
 
-
-itos = {i:s for s,i in stoi.items()}
-
 plt.figure(figsize=(16,16))
 plt.imshow(N, cmap='Blues')
-for i in range(28):
-    for j in range(28):
+for i in range(27):
+    for j in range(27):
         chstr = itos[i] + itos[j]
         plt.text(j,i,chstr, ha="center", va="bottom", color="gray")
         plt.text(j, i, N[i,j].item(), ha="center", va="top", color="gray")
 plt.axis("off")
 plt.show()
+
+ 
